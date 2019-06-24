0e518a2f6479f7de0da751bc9c0712a297e12a60
Stephen Jennings <stephen.g.jennings@gmail.com>
2019-06-24 10:21:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Turbocharged.Sudo
+=================
+
+A .NET Global Tool that adds a "sudo" command for Windows. This launches the
+command with an elevated token.
---
+++ sudo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                    throw new ArgumentException("Must provide a command to execute.");
+
+                var fileName = args[0];
+                var arguments = string.Join(" ", args.Skip(1));
+
+                var psi = new ProcessStartInfo
+                {
+                    FileName = fileName,
+                    Arguments = arguments,
+                    Verb = "runas",
+                    UseShellExecute = true,
+                    WorkingDirectory = Environment.CurrentDirectory,
+                };
+                var proc = Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+        }
+    }
+}
